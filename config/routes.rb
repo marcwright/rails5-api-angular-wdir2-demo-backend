12f6d9adc5241720e0c6d8f1a8046230ba3dd9fd
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2016-10-26 18:08:08 -0400

    added patient and doctors controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :doctors, path: '/api/doctors'
+  resources :patients, path: '/api/patients'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
